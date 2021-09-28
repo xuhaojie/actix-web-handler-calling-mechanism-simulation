724eb17dbb4b31cec3918b8b2441850b86957927
xuhaojie <xuhaojie@hotmail.com>
2021-09-28 23:12:07 +0800

    make things better

--- src/main.rs
+++ src/main.rs
@@ -1,5 +1,5 @@
 pub mod http {
-	#[derive(Copy, Clone, PartialOrd, PartialEq)]
+	#[derive(Copy, Clone, PartialOrd, PartialEq, Debug)]
 	pub enum Method {
 		Get,
 		Post,
@@ -8,17 +8,26 @@ pub mod http {
 	}
 
 	pub struct HttpRequest {
-		pub method: Method,
-		pub path: &'static str,
-		pub body: &'static str,
+		method: Method,
+		path: &'static str,
+		body: &'static str,
 	}
 
 	impl HttpRequest {
-		fn method(&self) -> Method {
+		pub fn new(method: Method, path: &'static str, body: &'static str) -> Self{
+			HttpRequest{method, path, body}
+		}
+
+		pub fn method(&self) -> Method {
 			self.method.clone()
 		}
-		fn path(&self) -> &'static str {
+
+		pub fn path(&self) -> &'static str {
 			self.path
+		}
+
+		pub fn body(&self) -> &'static str {
+			self.body
 		}
 	}
 
@@ -35,19 +44,19 @@ trait FromRequest {
 
 impl FromRequest for i32 {
 	fn from_request(req: &HttpRequest) -> Self {
-		req.body.parse().unwrap()
+		req.body().parse().unwrap()
 	}
 }
 
 impl FromRequest for f64 {
 	fn from_request(req: &HttpRequest) -> Self {
-		req.body.parse().unwrap()
+		req.body().parse().unwrap()
 	}
 }
 
 impl FromRequest for String {
 	fn from_request(req: &HttpRequest) -> Self {
-		"string".to_string()
+		req.body().to_string()
 	}
 }
 
@@ -92,7 +101,6 @@ trait HandlerFactory<P, R> {
 	}
 }
 
-
 impl<T, R> HandlerFactory<(),R> for T
 	where
 		R: Responder,
@@ -105,8 +113,8 @@ impl<T, R> HandlerFactory<(),R> for T
 
 impl<T, R, A> HandlerFactory<(A,),R> for T
 	where
-		R: Responder,
 		T: Fn(A) -> R,
+		R: Responder,
 		A: FromRequest,
 {
 	fn exec(&self, req: &HttpRequest) -> HttpResponse {
@@ -117,8 +125,8 @@ impl<T, R, A> HandlerFactory<(A,),R> for T
 
 impl<T, R, A, B> HandlerFactory<(A, B),R> for T
 	where
-		R: Responder,
 		T: Fn(A, B) -> R,
+		R: Responder,
 		A: FromRequest,
 		B: FromRequest,
 {
@@ -131,8 +139,8 @@ impl<T, R, A, B> HandlerFactory<(A, B),R> for T
 
 impl<T, R, A, B, C> HandlerFactory<(A, B, C),R> for T
 	where
-		R: Responder,
 		T: Fn(A, B, C) -> R,
+		R: Responder,
 		A: FromRequest,
 		B: FromRequest,
 		C: FromRequest,
@@ -187,105 +195,63 @@ impl Dispatcher {
 	fn dispatch(&self, req: &HttpRequest) {
 		let mut find = false;
 		for r in self.handlers.iter() {
-			if r.method == req.method && r.path == req.path {
+			if r.method == req.method() && r.path == req.path() {
 				find = true;
 				let h = &r.handler;
-				//let rsp = h.exec(req).body;
 				let rsp = h(&req);
-				println!("response to {}: {}", r.path, rsp.body);
+				println!("response to {:?} {}: {}", r.method, r.path, rsp.body);
 			}
 		}
 		if !find {
-			println!("response to {}: handler not find",req.path);
+			println!("response to {:?} {}: handler not find",req.method(), req.path());
 		}
 	}
 }
 
 // no parameter, no return value
-fn test0() {
+fn hello() {
 
 }
 
 // no parameter, return i32
-fn test01() -> i32 {
-	1
+fn hi() -> i32 {
+	2021
 }
 
 // i32 parameter, return i32
-fn test1(i: i32) -> String {
-	i.to_string()
+fn login(name: String, password: String) -> String {
+	format!("{}-{}", name, password)
 }
 
 // String, f64 parameters, return i32
-fn test2(msg: String, f: f64) -> String {
+fn new_user(msg: String, f: f64) -> String {
 	msg.clone()
 }
 
 // i32, String parameters, f64 return
-fn test2a(i: i32, msg: String) -> f64 {
+fn logout(i: i32, msg: String) -> f64 {
 	(i as f64)
 }
 
 // String, f64, i32 parameters, f64 return
-fn test3(msg: String, f: f64, i: i32) -> f64 {
+fn list_user(msg: String, f: f64, i: i32) -> f64 {
 	f
 }
 
 fn main() {
 	let mut d = Dispatcher::new();
-	d.add(http::Method::Get, "/hello", &test0);
-	d.add(http::Method::Get, "/hi", &test01);
-	d.add(http::Method::Get, "/login", &test1);
-	d.add(http::Method::Post, "/users/1", &test2);
-	d.add(http::Method::Put, "/logout", &test2a);
-	d.add(http::Method::Delete, "/users", &test3);
+	d.add(http::Method::Get, "/hello", &hello);
+	d.add(http::Method::Get, "/hi", &hi);
+	d.add(http::Method::Get, "/login", &login);
+	d.add(http::Method::Post, "/users/1", &new_user);
+	d.add(http::Method::Put, "/logout", &logout);
+	d.add(http::Method::Delete, "/users", &list_user);
 
-	let mut req0 = HttpRequest {
-		method: Method::Get,
-		path: "/hello",
-		body: "",
-	};
-	d.dispatch(&req0);
-
-	let mut req01 = HttpRequest {
-		method: Method::Get,
-		path: "/hi",
-		body: "",
-	};
-	d.dispatch(&req01);
-
-	let mut req1 = HttpRequest {
-		method: Method::Get,
-		path: "/login",
-		body: "123",
-	};
-	d.dispatch(&req1);
-
-	let mut req2 = HttpRequest {
-		method: Method::Post,
-		path: "/users/1",
-		body: "456",
-	};
-	d.dispatch(&req2);
-
-	let mut req2a = HttpRequest {
-		method: Method::Put,
-		path: "/logout",
-		body: "456",
-	};
-	d.dispatch(&req2a);
-
-	let mut req3 = HttpRequest {
-		method: Method::Delete,
-		path: "/users",
-		body: "789",
-	};
-	d.dispatch(&req3);
-
-	let mut req4 = HttpRequest {
-		method: Method::Delete,
-		path: "/notexist",
-		body: "789",
-	};
-	d.dispatch(&req4);
+	d.dispatch(&HttpRequest::new(Method::Get, "/hello",""));
+	d.dispatch(&HttpRequest::new(Method::Get, "/hi", ""));
+	d.dispatch(&HttpRequest::new(Method::Get,"/login","123"));
+	d.dispatch(&HttpRequest::new(Method::Post,"/users/1","456"));
+	d.dispatch(&HttpRequest::new(Method::Put, "/logout","456"));
+	d.dispatch(&HttpRequest::new(Method::Delete,"/users","789"));
+	d.dispatch(&HttpRequest::new(Method::Delete, "/not-exist","789"));
 }
